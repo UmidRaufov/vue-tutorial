a3c87aa0ae64d234a9bd7c42dadb6a932fb413f2
programmer <raufovdev@gmail.com>
2022-08-10 17:59:01 +0500

    step1

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <comment-form />
+    <comment-list v-bind:comments="comments" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CommentForm  from './components/CommentForm';
+import CommentList  from './components/CommentList';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    CommentForm,
+    CommentList,
+  },
+  data() {
+    return {
+      comments: [
+        {id: 1, name: "Jon  Smit", email: "1mail", content: "Lorem1"},
+        {id: 2, name: "Karl Smit", email: "2mail", content: "Lorem2"},
+        {id: 3, name: "Jeki Smit", email: "3mail", content: "Lorem3"},
+        {id: 4, name: "Joan Smit", email: "3mail", content: "Lorem4"},
+      ]
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1
+    },
+    decrement() {
+      this.count -= 1
+    },
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+
 </style>
---
+++ src/components/CommentForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="w-75 mx-auto">
+      <h3>Add Comments</h3>
+      <form @submit.prevent>
+        <input
+            v-model="comment.name"
+            placeholder="Enter Your Name..."
+            class="form-control"/>
+        <input
+            v-model="comment.email"
+            placeholder="Enter Your Email..."
+            class="form-control my-3"/>
+        <input
+            v-model="comment.content"
+            placeholder="Enter Your Content..."
+            class="form-control"/>
+        <div class="d-flex flex-row-reverse">
+          <button @click="createComment" class="btn btn-primary mt-3">Add Comment</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comment: {
+        name: '',
+        email: '',
+        content: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
